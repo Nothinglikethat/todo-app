6595bf8312543c7b636b6f25a5256c9023ac5f75
NothingLikeThat <fdsdf_fd@mail.ru>
2023-12-09 21:53:52 +0300

    added user registration

--- cmd/main.go
+++ cmd/main.go
@@ -1,16 +1,53 @@
 package main
 
 import (
-	"log"
+	"os"
 
 	"github.com/Nothinglikethat/todo-app"
 	"github.com/Nothinglikethat/todo-app/pkg/handler"
+	"github.com/Nothinglikethat/todo-app/pkg/repository"
+	"github.com/Nothinglikethat/todo-app/pkg/service"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
 )
 
 func main() {
-	handlers := new(handler.Handler)
-	srv := new(todo.Server)
-	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
-		log.Fatalf("error occured while runing http server: %s", err.Error())
+	logrus.SetFormatter(new(logrus.JSONFormatter))
+
+	if err := initConfig(); err != nil {
+		logrus.Fatalf("error initializing configs: %s", err.Error())
 	}
+
+	if err := godotenv.Load(); err != nil {
+		logrus.Fatalf("error loading env variables: %s", err.Error())
+	}
+
+	db, err := repository.NewPostgresDB(repository.Config{
+		Host:     viper.GetString("db.host"),
+		Port:     viper.GetString("db.port"),
+		Username: viper.GetString("db.username"),
+		DBName:   viper.GetString("db.dbname"),
+		SSLMode:  viper.GetString("db.sslmode"),
+		Password: os.Getenv("DB_PASSWORD"),
+	})
+	if err != nil {
+		logrus.Fatalf("failed to initialized db: %s", err.Error())
+	}
+
+	repos := repository.NewRepository(db)
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
+
+	srv := new(todo.Server)
+	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
+		logrus.Fatalf("error occured while runing http server: #{err.Error()}")
+	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
---
+++ pkg/service/service.go
@@ -0,0 +1,28 @@
+package service
+
+import (
+	"github.com/Nothinglikethat/todo-app"
+	"github.com/Nothinglikethat/todo-app/pkg/repository"
+)
+
+type Authorization interface {
+	CreateUser(user todo.User) (int, error)
+}
+
+type TodoList interface {
+}
+
+type TodoItem interface {
+}
+
+type Service struct {
+	Authorization
+	TodoList
+	TodoItem
+}
+
+func NewService(repos *repository.Repository) *Service {
+	return &Service{
+		Authorization: NewAuthService(repos.Authorization),
+	}
+}
